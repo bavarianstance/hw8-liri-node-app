6dd5f76b27ece2a9f38ba63bc297645f7817693a
bavarianstance <bavarianstance@gmail.com>
2019-03-20 13:22:37 -0700

    incremental update, added features

--- liri.js
+++ liri.js
@@ -5,8 +5,10 @@ const keys = require("./keys.js");
 const moment = require("moment");
 const fs = require("fs");
 const axios = require("axios");
+const figlet = require("figlet");
 const Spotify = require("node-spotify-api");
 const spotify = new Spotify(keys.spotify);
+const divider = "\n------------------------------------------------------------\n\n";
 
 //capture user inputs
 let inputFunction = process.argv[2];
@@ -24,17 +26,40 @@ for (let i = 3; i < nodeArgs.length; i++) {
 }
 
 const runConcertSearchApp = (inputArgument) => {
+
+	figlet('Concert This!', (err, data) => {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
+		if (inputArgument === "") {
+			inputArgument = "Red Hot Chili Peppers";
+		}
 	let queryUrl = "https://rest.bandsintown.com/artists/" + inputArgument + "/events?app_id=codingbootcamp";
 	axios.get(queryUrl)
 	.then(
 		(response) => {
-			console.log("--- CONCERT INFO ---")
 			for (let i = 0; i < response.data.length; i++){
-			console.log("---RESULT #: " + (i+1) + " ---")
- 			console.log("Name of venue: " + response.data[i].venue.name);
-			console.log("Location of venue: " + response.data[i].venue.city);
-			console.log("Date of the Event: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
-			console.log("Artist Lineup: " + response.data[i].lineup);
+			console.log(divider);
+			// console.log("---RESULT #: " + (i+1) + " ---")
+ 		// 	console.log("Name of venue: " + response.data[i].venue.name);
+			// console.log("Location of venue: " + response.data[i].venue.city);
+			// console.log("Date of the Event: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
+			// console.log("Artist Lineup: " + response.data[i].lineup);
+			let responseData = [
+			"---RESULT #: " + (i+1) + " ---",
+			"Name of venue: " + response.data[i].venue.name,
+			"Location of venue: " + response.data[i].venue.city,
+			"Date of the Event: " + moment(response.data[i].datetime).format("MM/DD/YYYY"),
+			"Artist Lineup: " + response.data[i].lineup
+			].join("\n\n");
+			console.log(responseData);
+			fs.appendFile("log.txt", responseData + divider, (err) => {
+				if (err) throw err;
+			});
 		}
 		}).catch((error) => {
     	 console.log(error);
@@ -42,6 +67,15 @@ const runConcertSearchApp = (inputArgument) => {
 }
 
 const runMovieSearchApp = (inputArgument) => {
+
+	figlet('Movie This!', (err, data) => {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
 		if (inputArgument === "") {
 			inputArgument = "Mr. Nobody";
 		}
@@ -49,19 +83,40 @@ const runMovieSearchApp = (inputArgument) => {
 		axios.get(queryUrl)
 		.then(
 			(response) => {
-			console.log("Title of the movie: " + response.data.Title);
-			console.log("Year of Release: " + response.data.Year);
-			console.log("IMDB Rating :" + response.data.imdbRating);
-			console.log("RottenTomatoes Rating: " + response.data.Ratings[1].Value);
-			console.log("Original Filmed Language: " + response.data.Language);
-			console.log("Sypnosis: " + response.data.Plot);
-			console.log("Top Billed Cast: " + response.data.Actors);			
-		}).catch((error) => {
+			let responseData = [
+			"Title of the movie: " + response.data.Title,
+			"Year of Release: " + response.data.Year,
+			"IMDB Rating :" + response.data.imdbRating,
+			"RottenTomatoes Rating: " + response.data.Ratings[1].Value,
+			"Original Filmed Language: " + response.data.Language,
+			"Sypnosis: " + response.data.Plot,
+			"Top Billed Cast: " + response.data.Actors
+			].join("\n\n");
+			console.log(responseData);
+			// console.log("Title of the movie: " + response.data.Title);
+			// console.log("Year of Release: " + response.data.Year);
+			// console.log("IMDB Rating :" + response.data.imdbRating);
+			// console.log("RottenTomatoes Rating: " + response.data.Ratings[1].Value);
+			// console.log("Original Filmed Language: " + response.data.Language);
+			// console.log("Sypnosis: " + response.data.Plot);
+			// console.log("Top Billed Cast: " + response.data.Actors);		
+			fs.appendFile("log.txt", responseData + divider, (err) => {
+				if (err) throw err;
+			});
+			}).catch((error) => {
 			console.log(error);
 		});
 }
 
 const runSongSearchApp = (inputArgument) => {
+	figlet('Spotify This!', (err, data) => {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
 	if (inputArgument === ""){
 		inputArgument = "The Sign Ace of Base";
  	}
@@ -78,11 +133,23 @@ const runSongSearchApp = (inputArgument) => {
  	let results = data.tracks.items;
  	console.log("---SONG INFO---");
  	for (let i = 0; i < results.length; i++){
- 	 console.log("---RESULT #: " + (i+1) + " ---");
-     console.log("Artist/Band: " + results[i].artists[0].name); 
-     console.log("Song name: " + results[i].name);
- 	 console.log("Preview song url: " + results[i].preview_url); 		
- 	 console.log("Album name: " + results[i].album.name);
+ 	 console.log(divider);
+ 	 let responseData = [
+ 	 "---RESULT #: " + (i+1) + " ---",
+ 	 "Artist/Band: " + results[i].artists[0].name,
+ 	 "Song name: " + results[i].name,
+ 	 "Preview song url: " + results[i].preview_url,
+ 	 "Album name: " + results[i].album.name
+ 	 ].join("\n\n");
+ 	 console.log(responseData);
+ 	 fs.appendFile("log.txt", responseData + divider, (err) => {
+				if (err) throw err;
+	 });
+ 	 // console.log("---RESULT #: " + (i+1) + " ---");
+   //   console.log("Artist/Band: " + results[i].artists[0].name); 
+   //   console.log("Song name: " + results[i].name);
+ 	 // console.log("Preview song url: " + results[i].preview_url); 		
+ 	 // console.log("Album name: " + results[i].album.name);
  	  }
  	}
   )
